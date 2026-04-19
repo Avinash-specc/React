a3e30528cf927961c28e4ffdc0af34e1bf065b3f
Avinash Kushwaha <avinashkushwaha7265@gmail.com>
2026-04-19 08:01:38 +0530

    Added Style

--- Ash/src/App.jsx
+++ Ash/src/App.jsx
@@ -8,7 +8,7 @@ function Counter() {
   const [count,setCount] = useState(0);
   return (
     <>
-      <div>
+      <div className='container'>
         <p>Count: {count}</p>
         <button onClick={()=> setCount(count+1)}>Increment</button>
         <button onClick={()=> setCount(count-1)}>Decrement</button>
